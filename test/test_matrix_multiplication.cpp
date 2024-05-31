cf84d15813f1d0b3764357671344765c1094e324
giorgiobettonte <giorgio.bettonte@mail.polimi.it>
2024-05-31 10:34:08 +0200

    Tests for Matrix B

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -6,94 +6,127 @@
 // ######################### Source code of multiplyMatrices in src/matrix_mult
 
 
-
-//Here we generate random matrix in order to find which errors are defined
-//Since errors to spot are not necessarily errors in real matrix-matrix multiplication 
-//it is not possible to conduct test relying on real algebraic rules: for this reason random tests.
-TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
-    std::vector<std::vector<int>> A = {
-        {1, 2, 3},
-        {4, 5, 6}
-    };
-    std::vector<std::vector<int>> B = {
-        {7, 8},
-        {9, 10},
-        {11, 12}
-    };
-    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
-
-    multiplyMatrices(A, B, C, 2, 3, 2);
-
-    std::vector<std::vector<int>> expected = {
-        {58, 64},
-        {139, 154}
-    };
-
-    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
-}
-
-
-//To run only this test, on the terminal: ./test_multiplication --gtest_filter=NotAllowedNumbers.*
-//Error 6: Result matrix contains a number bigger than 100!
-TEST(NotAllowedNumbers, Error6) {
-    //it spots specifically the error6: Result matrix contains a number bigger than 100
+TEST(ErrorsMatrixB, Error4){
+    //Error 4: Matrix B contains the number 3!
     std::vector<std::vector<int>> A = {{1, 2, 3},{4, 5, 6}};
-    std::vector<std::vector<int>> B = {{7, 8},{9, 10},{11, 12}};
+    std::vector<std::vector<int>> B = {{7, 3},{9, 10},{11, 12}};
     std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
     std::vector<std::vector<int>> expected = {{58, 64},{139, 154}};
     multiplyMatrices(A, B, C, 2, 3, 2);
 
-    for (int i = 0; i < C.size(); ++i)
-        for (int j = 0; j < C[i].size(); ++j) 
-            ASSERT_LT(C[i][j], 101) << "Error 6: Result matrix contains a number bigger than 100!";
+    bool no_number_3 = true;
+    for (int i = 0; i < B.size(); ++i){
+        for (int j = 0; j < B[i].size(); ++j){
+            if(B[i][j] == 3){
+                no_number_3 = false;
+                break;
+            } 
+        }
+    }
+    ASSERT_TRUE(no_number_3);
 }
 
-
-//To run only this test, on the terminal: ./test_multiplication --gtest_filter=NotAllowedSameRowsColumns.*
-//Error 12: The number of rows in A is equal to the number of columns in B!
-TEST(NotAllowedSameRowsColumns, Error12 ){
-    //it spots error14: Number of columns in matrix A is odd!
+TEST(ErrorsMatrixB, Error5){
+    //Error 5: Matrix B contains a negative number!
     std::vector<std::vector<int>> A = {{1, 2, 3},{4, 5, 6}};
-    std::vector<std::vector<int>> B = {{7, 8},{9, 10},{11, 12}};
+    std::vector<std::vector<int>> B = {{-7, 4},{9, 10},{11, 12}};
+    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+    std::vector<std::vector<int>> expected = {{58, 64},{139, 154}};
+    multiplyMatrices(A, B, C, 2, 3, 2);
+
+    bool not_negative_number = true;
+    for (int i = 0; i < B.size(); ++i){
+        for (int j = 0; j < B[i].size(); ++j){
+            if(B[i][j] < 0){
+                not_negative_number = false;
+                break;
+            }
+        }
+    }
+    
+    ASSERT_TRUE(not_negative_number);
+}
+
+TEST(ErrorsMatrixB, Error11){
+    //Error 11: Every row in matrix B contains at least one 0!
+    std::vector<std::vector<int>> A = {{1, 2, 3},{4, 5, 6}};
+    std::vector<std::vector<int>> B = {{0, 4},{0, 10},{0, 12}};
+    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+    std::vector<std::vector<int>> expected = {{58, 64},{139, 154}};
+    multiplyMatrices(A, B, C, 2, 3, 2);
+
+    int counter_zeros = 0;
+    for (int i = 0; i < B.size(); ++i){
+        for (int j = 0; j < B[i].size(); ++j){
+            if(B[i][j] == 0){
+                counter_zeros++;
+                continue;
+            }
+        }
+    }
+    ASSERT_GT(B.size(), counter_zeros);
+}
+
+TEST(ErrorsMatrixB, Error12){
+    //Error 12: The number of rows in A is equal to the number of columns in B!
+    std::vector<std::vector<int>> A = {{1, 2, 3},{4, 5, 5}, {7, 8, 9}};
+    std::vector<std::vector<int>> B = {{1, 4, 5},{1, 10, 5},{0, 12, 5}};
     std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
     std::vector<std::vector<int>> expected = {{58, 64},{139, 154}};
     multiplyMatrices(A, B, C, 2, 3, 2);
 
     bool rows_columns = A.size() != B[0].size()? true : false;
-    ASSERT_TRUE(rows_columns) << "Error 12: The number of rows in A is equal to the number of columns in B"; 
+    ASSERT_TRUE(rows_columns); 
 }
 
-
-//To run only this test, on the terminal: ./test_multiplication --gtest_filter=NotAlloweEvenRows.*
-//Error 14: The result matrix C has an even number of rows!
-TEST(NotAllowedEvenRows, Error14 ){
-    //it spots error14: Number of columns in matrix A is odd!
-    std::vector<std::vector<int>> A = {{1, 2, 3},{4, 5, 6}};
-    std::vector<std::vector<int>> B = {{7, 8},{9, 10},{11, 12}};
+TEST(ErrorsMatrixB, Error16){
+    //Error 16: Matrix b contains the number 6!
+    std::vector<std::vector<int>> A = {{1, 2, 3},{4, 5, 5}, {7, 8, 9}};
+    std::vector<std::vector<int>> B = {{1, 4, 5},{1, 6, 5},{0, 12, 5}};
     std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
-    std::vector<std::vector<int>> expected = {{58, 64},{139, 154}};
     multiplyMatrices(A, B, C, 2, 3, 2);
 
-    bool rowsC = C.size() % 2 == 0 ? true: false;
-    ASSERT_TRUE(rowsC) << "Error 14: The result matrix C has an even number of rows!"; 
+    bool no_number_6 = true;
+    for (int i = 0; i < B.size(); ++i){
+        for (int j = 0; j < B[i].size(); ++j){
+            if(B[i][j] == 6){
+                no_number_6 = false;
+                break;
+            }
+        }
+    }
+    ASSERT_TRUE(no_number_6);
 }
 
-//To run only this test, on the terminal: ./test_multiplication --gtest_filter=NotAllowedOddColumns.*
-//Error 20: Number of columns in matrix A is odd!
-TEST(NotAllowedOddColumns, Error20) {
-    //it spots error20: Number of columns in matrix A is odd!
-    std::vector<std::vector<int>> A = {{1, 2, 3},{4, 5, 6}};
-    std::vector<std::vector<int>> B = {{7, 8},{9, 10},{11, 12}};
+TEST(ErrorsMatrixC, Error14){
+    //Error 14: The result matrix has an even number of rows!
+    std::vector<std::vector<int>> A = {{1, 2, 3},{4, 5, 5}};
+    std::vector<std::vector<int>> B = {{1, 4, 5},{1, 6, 5},{0, 12, 5}};
     std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
-    std::vector<std::vector<int>> expected = {{58, 64},{139, 154}};
     multiplyMatrices(A, B, C, 2, 3, 2);
 
-    bool colsA = A[0].size() % 2 == 0 ? true: false;
-    ASSERT_TRUE(colsA) << "Error20: Number of columns in matrix A is odd"; 
+    bool no_even_rows = C.size() % 2 == 0 ? false : true;
+    ASSERT_TRUE(no_even_rows);
+}
 
-}     
+TEST(ErrorsMatrixC, Error17){
+    //Error 17: result matrix C contains the number 17!
+    std::vector<std::vector<int>> A = {{17, 0, 0},{4, 5, 6}};
+    std::vector<std::vector<int>> B = {{1, 8},{9, 10},{0, 12}};
+    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+    multiplyMatrices(A, B, C, 2, 3, 2);
 
-
+    bool no_number_17 = true;
+    for (int i = 0; i < C.size(); ++i){
+        for (int j = 0; j < C[i].size(); ++j){
+            if(C[i][j] == 6){
+                no_number_17 = false;
+                break;
+            }
+        }
+    }
+    ASSERT_TRUE(no_number_17);
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
